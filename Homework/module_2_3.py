c38db86b524cf8bccd5abf0db62935ed528b36e9
den-zakalyapin <zakalyapin74@mail.ru>
2024-09-16 22:25:52 +0500

    Initial commit

---
+++ Homework/module_2_3.py
@@ -0,0 +1,38 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+positive_numbers = []
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        positive_numbers.append(my_list[i])
+    elif my_list[i] < 0:
+        break
+    i += 1
+print(positive_numbers)
+
+#my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#i = 0
+#while i < len(my_list) :
+    #if my_list[i] > 0:
+        #print(my_list[i])
+
+        #i += 1
+
+#my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#i = 0
+#while i <= len(my_list):
+    #if my_list[i] < 0:
+        #break
+    #elif my_list[i] > 0:
+        #print(my_list[i])
+    #i += 1
+
+#my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+#i = 0
+#while i < len(my_list) :
+    #if my_list[i] < 0:
+        #break
+
+    #elif my_list[i] > 0:
+        #print(my_list[i])
+    #i += 1
