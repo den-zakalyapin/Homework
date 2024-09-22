3930bbd5111c7d767679ce724fba03c23f9b7013
den-zakalyapin <zakalyapin74@mail.ru>
2024-09-22 18:55:52 +0500

    добавляем условие для еденицы,чтобы исключить её из цикла фор

--- Homework/module_2_4.py
+++ Homework/module_2_4.py
@@ -1,19 +1,23 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for number in numbers :
+
+
+for number in numbers:
+    if number == 1:#добавляем условие для еденицы,чтобы исключить её из цикла фор
+        continue
     is_prime = True
     for i in range(2, number):
-        if number % i ==0:
+        if number % i == 0:
             is_prime = False
             break
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
 
-    if is_prime and number != 1:
-        primes.append (number)
-    else :
-        not_primes.append (number)
-print('prime numbers:',primes)
-print('not prime numbers:', not_primes)
+print("Primes:", primes)
+print("Not Primes:", not_primes)
 
 
 
