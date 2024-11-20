e5b808c4b6641dff93981e48a1a3fcc1a75f762a
den-zakalyapin <zakalyapin74@mail.ru>
2024-11-20 19:21:43 +0500

    +

--- pythonProject 6 modules/module_6_1.py
+++ pythonProject 6 modules/module_6_1.py
@@ -1,39 +1,75 @@
+# class Animal:
+#     def __init__(self, name):
+#         self.alive = True
+#         self.fed = False
+#         self.name = name
+#
+# class Plant:
+#     def __init__(self, name):
+#         self.edible = False
+#         self.name = name
+#
+# class Mammal(Animal):
+#     def eat(self, food):
+#         if isinstance(food, Plant) and food.edible:
+#             print(f"{self.name} съел {food.name}")
+#             self.fed = True
+#         else:
+#             print(f"{self.name} не стал есть {food.name}")
+#             self.alive = False
+#
+# class Predator(Animal):
+#     def eat(self, food):
+#         if isinstance(food, Plant) and food.edible:
+#             print(f"{self.name} съел {food.name}")
+#             self.fed = True
+#         else:
+#             print(f"{self.name} не стал есть {food.name}")
+#             self.alive = False
+# class Flower(Plant):
+#     pass
+#
+# class Fruit(Plant):
+#     def __init__(self, name):
+#        super().__init__(name)
+#        self.edible = True
+
 class Animal:
+    alive = True  # Атрибут класса
+    fed = False  # Атрибут класса
+
     def __init__(self, name):
-        self.alive = True
-        self.fed = False
-        self.name = name
+        self.name = name  # Атрибут экземпляра
+
+    def eat(self, food):
+        if isinstance(food, Plant) and food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.__class__.fed = True #изменение атрибута класса
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.__class__.alive = False #изменение атрибута класса
+
 
 class Plant:
+    edible = False  # Атрибут класса
+
     def __init__(self, name):
-        self.edible = False
-        self.name = name
+        self.name = name  # Атрибут экземпляра
+
 
 class Mammal(Animal):
-    def eat(self, food):
-        if isinstance(food, Plant) and food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if isinstance(food, Plant) and food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass
+
 class Flower(Plant):
     pass
 
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
-        self.edible = True
-
+        self.edible = True  # Переопределение атрибута для экземпляра
 
 
 
