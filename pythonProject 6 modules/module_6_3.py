d856b0de5c2ff7d62971bef8e9ef53d16eea5332
den-zakalyapin <zakalyapin74@mail.ru>
2024-11-27 21:28:25 +0500

    +

---
+++ pythonProject 6 modules/module_6_3.py
@@ -0,0 +1,74 @@
+import random
+
+class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
+    def __init__(self, speed, cords = [0, 0, 0]):
+        self.speed = speed
+        self._cords = cords
+
+    def move(self, dx, dy, dz):
+        new_x = self._cords[0] + dx * self.speed
+        new_y = self._cords[1] + dy * self.speed
+        new_z = self._cords[2] + dz * self.speed
+        if new_z < 0:
+            print("It's too deep, i can't dive :(")
+            new_z = 0
+        self._cords = [new_x, new_y, new_z]
+
+    def get_cords(self):
+        return f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}"
+    def attack(self):
+        if self._DEGREE_OF_DANGER >= 5:
+            print("Be careful, i'm attacking you 0_0" )
+        else:
+            print("Sorry, i'm peaceful :)")
+
+    def speak(self):
+        if self.sound:
+            print(self.sound)
+        else:
+            print("I don't know how to speak")
+
+class Bird(Animal):
+    beak = True
+    def lay_eggs(self):
+        num_eggs = random.randint(1,4)
+        print(f"Here are(is) {num_eggs} eggs for you")
+
+class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+    def dive_in(self, dz):
+        new_z = self._cords[2] - abs(dz) * (self.speed / 2)  # скорость уменьшена вдвое
+        self._cords[2] = new_z
+
+class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
+class Duckbill(Bird, PoisonousAnimal,AquaticAnimal):
+    sound = "Click-click-click"
+
+    def __init__(self, speed):
+        super().__init__(speed)
+
+
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+
+db.get_cords()
+db.dive_in(6)
+
+db.get_cords()
+print(db.get_cords())
+print(db.get_cords())
+db.lay_eggs()
